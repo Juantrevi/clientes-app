eecf8041d9a9aa7f869e17d24e126db2931f3a1a
Juantrevi <juantrevi70@gmail.com>
2023-10-06 20:14:21 +0100

    Footer - 1.0.0

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     // styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-    // Add any properties or methods you need for your navbar component
+    title: string  = 'App Angular 16';
 }
