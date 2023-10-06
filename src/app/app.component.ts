381fde731bdbf044dbcc2f6126ab0a601a0372a7
Juantrevi <juantrevi70@gmail.com>
2023-10-06 12:54:22 +0100

    1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Welcome to Angular 16';
+  course: string = 'Course of Angular 16';
+  teacher: string = 'Juan Manuel Treviranus';
 }
