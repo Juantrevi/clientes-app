38766cc2b03cf9ea0ec3c193ab2df0bd97722c7f
Juantrevi <juantrevi70@gmail.com>
2023-10-12 15:30:48 +0100

    -- Paginator full --

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,17 +1,44 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit{
+export class PaginatorComponent implements OnInit, OnChanges{
    @Input() paginatorChild: any;
 
    pages: number[];
 
+   from: number;
+    to: number;
+
    constructor() { }  
 
     ngOnInit(): void {
-      this.pages = new Array(this.paginatorChild.totalPages).fill(0).map((_value, index) => index + 1);
+      this.initPaginator();
+    }
+
+    ngOnChanges(change: SimpleChanges): void {
+      let updatedPaginator = change['paginatorChild'];
+
+      if (updatedPaginator.previousValue) {
+        this.initPaginator();
+      }
+    }
+
+    
+    public initPaginator(): void {
+      this.from = Math.min(Math.max(1, this.paginatorChild.number - 4), this.paginatorChild.totalPages - 5);
+      this.to = Math.max(Math.min(this.paginatorChild.totalPages, this.paginatorChild.number + 4), 6);
+
+      if (this.paginatorChild.totalPages > 5) {
+        this.pages = new Array(this.to - this.from + 1).fill(0).map((_value, index) => index + this.from);
+
+
+      } else {
+
+        this.pages = new Array(this.paginatorChild.totalPages).fill(0).map((_value, index) => index + 1);
+
+      }
     }
 }
